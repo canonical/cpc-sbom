84d263b49496a5269e864dfff5fce082e522126d
Philip Roche <phil.roche@canonical.com>
2022-11-29 18:18:04 +0000

    feat: Use debian.copyright.Copyright module to parse Copyright files to get package licences
    Note that not all copyright files are machine readable. If the copyright file is not machine readable,
    then we will skip the file and move on to the next package if --ignore-copyright-parsing-errors is set.
    If --ignore-copyright-parsing-errors is not set, then we will raise an exception and exit.
    If the copyright file is not found, then we will skip the file and move on to the next package if
    --ignore-copyright-file-not-found-errors is set. If --ignore-copyright-file-not-found-errors is not set,
    then we will raise an exception and exit.
    
    `cpc-sbom --ignore-copyright-parsing-errors --ignore-copyright-file-not-found-errors` will ignore missing
    copyright files and ignore copyright parsing errors and will proceed to produce an SBOM.

--- cpc_sbom/__init__.py
+++ cpc_sbom/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import json
 
+import argparse
 import apt
-import re
 
 from datetime import datetime
+from debian.copyright import Copyright, NotMachineReadableError
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -14,6 +15,10 @@ def _parser():
                                           'you wish to generate an SBOM for. This is useful if you are generating '
                                           'an SBOM for a mounted filesystem rather than the host. '
                                           'Default: %(default)s', default='/')
+    parser.add_argument('--ignore-copyright-parsing-errors', help='Ignore copyright parsing errors. ',
+                        action='store_true')
+    parser.add_argument('--ignore-copyright-file-not-found-errors', help='Ignore copyright file not found errors. ',
+                        action='store_true')
     return parser
 
 def generate_sbom():
@@ -29,37 +34,56 @@ def generate_sbom():
     installed_packages = []
     for package in cache:
         if package.is_installed:
+            package_name = package.name
+            package_shortname = package.shortname
             package_installed_files = package.installed_files
-            package_copyright_file = None
             package_copyright = ""
             package_licenses = []
-            # find the copyright file in the installed files list
-            for package_installed_file in package_installed_files:
-                if package_installed_file.endswith("copyright"):
-                    package_copyright_file = package_installed_file
-                    break
-            # if the copyright file is found, read the file and get the sha256
-            if package_copyright_file:
-                with open(
-                    package_copyright_file, "r", encoding="utf-8", errors="ignore"
-                ) as copyright_file:
-                    package_copyright = copyright_file.read()
+            # find the copyright file in filesystem
+            package_copyright_file = "{}usr/share/doc/{}/copyright".format(rootdir, package_shortname)
 
-            # use regular expression to get the Licenses from the copyright file
-            if package_copyright:
-                package_licenses = re.findall(
-                    r"^License: (.*)$\n", package_copyright, re.MULTILINE
-                )
-                package_licenses = list(set(package_licenses))
-                # strip any whitespace from the licenses
-                package_licenses = [
-                    package_license.strip()
-                    for package_license in package_licenses
-                    if package_license.strip() != ""
-                ]
+            # if the copyright file is found and the file exists, read the file and get the license information
+            # Note that not all copyright files are machine readable. If the copyright file is not machine readable,
+            # then we will skip the file and move on to the next package if --ignore-copyright-parsing-errors is set.
+            # If --ignore-copyright-parsing-errors is not set, then we will raise an exception and exit.
+            # If the copyright file is not found, then we will skip the file and move on to the next package if
+            # --ignore-copyright-file-not-found-errors is set. If --ignore-copyright-file-not-found-errors is not set,
+            # then we will raise an exception and exit.
+            try:
+                with open(package_copyright_file, 'rt', encoding='utf-8', errors="ignore") as copyright_file:
+                    package_copyright_object = Copyright(copyright_file, strict=False)
+                    all_copyright_paragraphs = package_copyright_object.all_paragraphs()
+                    for copyright_paragraph in all_copyright_paragraphs:
+                        if copyright_paragraph.license and copyright_paragraph.license.synopsis \
+                                and copyright_paragraph.license.synopsis.strip():
+                            package_licenses.append(copyright_paragraph.license.synopsis.strip())
+            except ValueError as copyright_value_error:
+                print("Unable to parse copyright file for package {} - {}: {}".format(
+                    package_name,
+                    package_copyright_file,
+                    copyright_value_error))
+                if not args.ignore_copyright_parsing_errors:
+                    raise copyright_value_error
+            except NotMachineReadableError as copyright_not_machine_readable_error:
+                print("Copyright file for package {} is not machine readable - {}: {}".format(
+                    package_name,
+                    package_copyright_file,
+                    copyright_not_machine_readable_error))
+                if not args.ignore_copyright_parsing_errors:
+                    raise copyright_not_machine_readable_error
+            except FileNotFoundError as copyright_file_not_found_error:
+                # raise an exception if the copyright file is not found
+                print("Copyright file not found for package {} - {}: {}".format(
+                    package_name,
+                    package_copyright_file,
+                    copyright_file_not_found_error))
+                if not args.ignore_copyright_file_not_found_errors:
+                    raise copyright_file_not_found_error
+
+            # ensure that package_licenses is a unique list
+            package_licenses = list(set(package_licenses))
 
             package_installed_record = package.installed.record
-            package_name = package.name
             package_version = package.installed.version
             package_architecture = package.installed.architecture
             package_sha256 = package_installed_record.get("SHA256", "")
