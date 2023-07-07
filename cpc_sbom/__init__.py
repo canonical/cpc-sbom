dc1be6dd89e82760f49aca2aaddd3b3812ff9035
Philip Roche <phil.roche@canonical.com>
2023-07-07 08:59:15 +0100

    Bump version: 0.1.10 → 0.1.11

--- cpc_sbom/__init__.py
+++ cpc_sbom/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-CPC_SBOM_VERSION = "0.1.10"
+CPC_SBOM_VERSION = "0.1.11"
 
 if __name__ == "__main__":
     from generate import generate_sbom
--- cpc_sbom/generate.py
+++ cpc_sbom/generate.py
@@ -16,7 +16,7 @@ from uuid import uuid4
 
 logger = logging.getLogger(__name__)
 
-CPC_SBOM_VERSION = "0.1.10"
+CPC_SBOM_VERSION = "0.1.11"
 
 
 def _parser():
